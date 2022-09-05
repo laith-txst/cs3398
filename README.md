5d547583b05514a5c6da3837e5c71a01985f2827
laith-txst <laithh@txstate.edu>
2022-09-05 13:24:25 -0500

    Some terrible bugs

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is a repository. I will make another change. Wee.
 Test 1, Test 2, Test 3.
 
 Beep. Boop.
+
+Oh no, we have some bugs... the good news is I know what they are!
+- type error (str/int)
+- no return statement
+- incorrect usage of append
--- test.py
+++ test.py
@@ -1,11 +1,27 @@
+# This function removes all even numbers from a given list
+# Input: A list of ints
+# Returns: A list of ints with all even numbers removed 
+def remove_all_even_numbers_from_list(original_list):
+    odd_number_list = []
+    odd_number_list.append([num for num in remaining_number_list if num % 2 != 0])
+
+
 one_to_ten_number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 reversed_one_to_ten_number_list = list(reversed(one_to_ten_number_list))
 
 for number in reversed_one_to_ten_number_list:
     if number != 3 and number != 7:
-        print(number, end = '')
+        print(f'{number} ', end = '')
 print()
 
 one_to_ten_set = set(reversed_one_to_ten_number_list)
 one_to_ten_set.remove(8)
 print(one_to_ten_set)
+
+remaining_number_list = list(map(lambda val: str(val),one_to_ten_set))
+print(remaining_number_list)
+
+
+# Remove all even numbers
+odd_number_list = remove_all_even_numbers_from_list(remaining_number_list)
+print(odd_number_list)
